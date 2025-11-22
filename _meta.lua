e18b1a8117f487e1e6b410f16c948cf6275c22bb
ömer faruk inel <omer.inel@ciellos.com>
2025-11-22 07:17:50 +0300

    v1.0.1

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "wordsearch",
     fullname = _([[Word Search]]),
     description = _([[Find hidden words inside a randomized letter grid.]]),
-    version = "1.0.0",
+    version = "1.0.1",
 }
