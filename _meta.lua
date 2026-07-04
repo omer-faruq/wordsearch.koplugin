5bfedf619d309d1c8976d33ddf5de008672aa34d
omerfarukinel <85738646+omer-faruq@users.noreply.github.com>
2026-07-04 17:29:12 +0300

    v1.1.0

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "wordsearch",
     fullname = _([[Word Search]]),
     description = _([[Find hidden words inside a randomized letter grid.]]),
-    version = "1.0.1",
+    version = "1.1.0",
 }
